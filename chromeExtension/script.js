5ca6689aa75d5af0149b82949260106c46626f46
brtowsky <Kfiusz@gmail.com>
2024-01-14 21:47:14 +0000

    Add files via upload

--- chromeExtension/script.js
+++ chromeExtension/script.js
@@ -1,48 +1,3 @@
-document.addEventListener('firstClick', function()
-{
-	var button1 = document.getElementById('timesheet-work-add-btn');
-	console.log("ELO");
-	button1.click();
-	
-});
-
-document.addEventListener('secondClick', function()
-{
-	var button2 = document.getElementById('Select Tasks');
-	//console.log(butAdd.textContent);
-	button2.click();
-	
-});
-
-document.addEventListener('thirdClick', function()
-{
-	
-	var button3 = document.getElementsByName('tasks-9458-timesheetTasks-common-search');
-	button3.value = textValue;
-	
-});
-
-
-
-				
-				function clickSecond()
-				{
-					var button2 = document.getElementById('Select Tasks');
-					//console.log(butAdd.textContent);
-					button2.click();
-					
-				}
-				
-				function setThird(textValue)
-				{
-					var button3 = document.getElementsByName('tasks-9458-timesheetTasks-common-search');
-					button3.value = textValue;
-					
-				}
-
-
-
-
 window.addEventListener("DOMContentLoaded", (event) => {
 	const el = document.getElementById('fixit');
 	var jobList = new Array();
@@ -210,24 +165,193 @@ if (el3) {
             const tab = tabs[0];
 
             function populate() {
-                function BclickFirst() {
-                    try {
+                function clickFirst() 
+				{
+                    try 
+					{
                         var button1 = document.getElementById('timesheet-work-add-btn');
-                        if (button1) {
+                        if (button1) 
+						{
                             button1.click();
-                        } else {
-                            console.error('Button with ID timesheet-work-add-btn not found.');
+                        } 
+						
+						else 
+						{
+                            console.log('Button with ID timesheet-work-add-btn not found.');
                         }
-                    } catch (error) {
-                        console.error('Error in BclickFirst:', error);
+                    }
+					
+					catch (error) 
+					{
+                        console.log('Error in clickFirst:', error);
                     }
                 }
+				
+				
+				
+				function clickSecond()
+				{
+					try
+					{
+						var button2 = document.getElementById('Select Tasks');
+						if (button2)
+						{
+							//console.log(butAdd.textContent);
+							button2.click();
+							
+						}
+						else
+						{
+							console.log('Button with ID Select Tasks found.');
+						}
+						
+					} catch(error)
+					{
+						console.log('Error in clickSecond: ', error);
+					}
+				}
+				
+				function selectmafield()
+				{
+					
+					
+						var selecField = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[2]/div/ppm-split/div[1]/div/ppm-grid-component/div/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div/div/div/div[1]/div/div/div/div[2]/input', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+						console.log("clicked", new Date().toLocaleTimeString());
+						selecField.click();
+						
+					
+					
+				}
+				
+				
+				
+				function checkIfExists(textValue)
+				{
+					
+					var isLoaded = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[2]/div/ppm-split/div[1]/div/ppm-grid-component/div/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div/div/div[1]/div[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+					
+					if (isLoaded)
+					{
+						console.log(isLoaded.textContent);
+						console.log("mytext:", isLoaded.textContent);
+						
+						
+						if (isLoaded.textContent.trim() == textValue)
+						{
+							console.log("mytext:", isLoaded.textContent, new Date().toLocaleTimeString());
+							selectmafield();
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else
+					{
+						console.log("f");
+						return false;
+					}
+				
+				}
+				
+				
+				function waitFor(delMs)
+				{ 
+					var TimeStart = Date.now();
+					console.log(TimeStart);
+					while ((Date.now() - TimeStart) <= delMs)
+					{
+						continue;
+					}
+					console.log(Date.now());
+					
+				}
+				
+				
+				function setThird(textValue)
+				{	
+					var FieldFound = false; 
+					try
+					{
+						var searchField = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[1]/div[4]/ppm-common-search/div/div/div/div/input', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+						if (searchField)
+						{//this will be a for each on jobList
+							
+							searchField.value = textValue;
+							var updateEvent = new Event ('change');
+							searchField.dispatchEvent(updateEvent);
+							var lvCount = 0;
+							var czasOd = Date.now();
+							var czasTeraz;
+							
+							
+							waitFor(5000);
+							FieldFound = checkIfExists();	
+							
+							console.log(FieldFound);	
+							
+														
+					
+							console.log("popopo", new Date().toLocaleTimeString());
+						
+							
+							
+							
+							
+						}
+						else
+						{
+							console.log('Could not find text field.');
+						}
+						
+					} 
+					catch(error)
+					{
+						console.log('Error in setThird: ', error);
+					}
+				}
+				
+				function selectJob()
+				{
+				try
+					{
+					//
+					var addButton = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[3]/ppm-button-st[2]/button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+					if (addButton)
+					{
+						
+							addButton.click();
+						
+						
+					}
+					else
+					{
+						console.log('Could not find add button.');
+					}
+					
+				} catch(error)
+				{
+					console.log('Error in selectJob: ', error);
+				}
+					
+					
+				}
 
-                BclickFirst();
+				
+				
+				
+				
+				
+				
 
-                var evt1 = document.createEvent('Event');
-                evt1.initEvent('firstClick', true, false);
-                document.dispatchEvent(evt1);
+                clickFirst();
+				
+				clickSecond();
+				
+				setThird("QAD - Other Busines Unit Support");
+
+                
             }
 
             chrome.scripting.executeScript({
