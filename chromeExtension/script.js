a39b05b53ddd7296e1f21e8128ea8927a8687ca2
brtowsky <Kfiusz@gmail.com>
2024-01-22 12:58:12 +0000

    Fully working save & populate.

--- chromeExtension/script.js
+++ chromeExtension/script.js
@@ -1,52 +1,7 @@
-document.addEventListener('firstClick', function()
+window.addEventListener("DOMContentLoaded", (event) => 
 {
-	var button1 = document.getElementById('timesheet-work-add-btn');
-	console.log("ELO");
-	button1.click();
-	
-});
-
-document.addEventListener('secondClick', function()
-{
-	var button2 = document.getElementById('Select Tasks');
-	//console.log(butAdd.textContent);
-	button2.click();
-	
-});
-
-document.addEventListener('thirdClick', function()
-{
-	
-	var button3 = document.getElementsByName('tasks-9458-timesheetTasks-common-search');
-	button3.value = textValue;
-	
-});
-
-
-
-				
-				function clickSecond()
-				{
-					var button2 = document.getElementById('Select Tasks');
-					//console.log(butAdd.textContent);
-					button2.click();
-					
-				}
-				
-				function setThird(textValue)
-				{
-					var button3 = document.getElementsByName('tasks-9458-timesheetTasks-common-search');
-					button3.value = textValue;
-					
-				}
-
-
-
-
-window.addEventListener("DOMContentLoaded", (event) => {
 	const el = document.getElementById('fixit');
 	var jobList = new Array();
-	
 	
 	if (el) 
 	{
@@ -54,8 +9,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 		el.addEventListener('click', ()  => {
 			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 				const tab = tabs[0];
-				
-				
 			
 				function getElementByXpath() {
 					
@@ -78,11 +31,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 						else
 						{
 							xx.remove();							
-						}
-						
-						
-							
-						
+						}						
 					}
 					
 					//check how many jobs there are
@@ -111,14 +60,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
 							{
 								continue; 
 							}
-													
-							
 						}
 						rownbr = zz;			
 					}
 								
 					
-					//row lopp
+					//row loop
 					for (let i = 2; i <= rownbr; i++) {
 						//column loop
 						for (let j = 1; j < 13; j++) {
@@ -150,12 +97,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
 								}								
 							}
 			
-							x.remove();
-			  
+							x.remove();			  
 						}
 					}			
-				  
-				}	
+				  }	
 			  
 
 				chrome.scripting.executeScript({
@@ -172,9 +117,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
 	
 	
 	const el2 = document.getElementById('saveList');
-	if (el2) {
-			el2.addEventListener('click', ()  => {
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	if (el2) 
+	{
+		el2.addEventListener('click', ()  => 
+		{
+			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => 
+			{
 				const tab = tabs[0];
 				
 				function SaveJobList()
@@ -184,8 +132,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 					
 					for (var i1 = 0 ; i1 < elLength; i1++)
 					{
-						jobList.push(elements.item(i1).textContent);
-						//console.log(elements.item(i1).textContent);						
+						jobList.push(elements.item(i1).textContent);						
 					}					
 				}
 			  
@@ -196,57 +143,201 @@ window.addEventListener("DOMContentLoaded", (event) => {
 					
 				});
 			});
-		});
-	
-	
+		});	
 	}
-	
 	
 	
 	const el3 = document.getElementById('populateList');
-	if (el3) {
-			el3.addEventListener('click', ()  => {
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	if (el3) 
+	{
+		el3.addEventListener('click', () => 
+		{
+			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => 
+			{
 				const tab = tabs[0];
 				
-				function BclickFirst()
+				async function populate() 
 				{
-					var button1 = document.getElementById('timesheet-work-add-btn');
-					button1.click();
+					function clickFirst() 
+					{
+						return new Promise((resolve,reject) =>
+						{
+							try 
+							{
+								var button1 = document.getElementById('timesheet-work-add-btn');
+								if (button1) 
+								{
+									button1.click();
+									resolve("clicked");		
+								} 
+								
+								else 
+								{
+									console.log('Button with ID timesheet-work-add-btn not found.');
+									reject('Button with ID timesheet-work-add-btn not found.');		
+								}
+							}
+							
+							catch (error) 
+							{
+								console.log('Error in clickFirst:', error);
+								reject('Button with ID timesheet-work-add-btn not found.');		
+							}
+						});
+	                }
+				
+				
+				
+				function clickSecond()
+				{
+					
+					return new Promise((resolve,reject) =>
+					{
+						setTimeout(function() 
+							{								
+								var button2 = document.getElementById('Select Tasks');
+								if (button2)
+								{
+									button2.click();	
+									resolve("clicked");								
+								}
+								else
+								{
+									console.log('Button Select Tasks not found.');
+									reject("button not found");								
+								}
+							}, 2000);
+						
+					});
+				}
+			
+				
+				
+				
+				function clickAdd()
+				{
+					try
+					{					
+						var addButton = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[3]/ppm-button-st[2]/button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+						if (addButton)
+						{						
+							addButton.click();											
+						}
+						else
+						{
+							console.log('Could not find add button.');
+						}				
+					} 
+					catch(error)
+					{
+						console.log('Error in selectJob: ', error);
+					}			
 					
 				}
-			  
-				
-				function populate()
-				{
-					var evt1 = document.createEvent('Event');
-					evt1.initEvent('firstClick', true, false);
-				
-				
-				
-					document.dispatchEvent(evt1);
-					BclickFirst();
-					
-					//clickSecond();
-					//setThird();
-				
-					
-										
-				}
+			
 				
 				
 
-				chrome.scripting.executeScript({
-					target: { tabId: tab.id },
-					func: populate
+				function mainFunc(elemNum)
+				{				
+					if (jobList.length == 0)
+					{   //last line defence again empty joblist.
+						console.log("joblist empty!");
+						return;					
+					}
 					
-				});
-			});
-		});
-	
-	
-	}
-	
-	
-	
-});
+					if (jobList[elemNum] == null)
+					{ //make sure element isnt't null ("undefined")
+						return;
+					}
+				
+					return new Promise ((resolve) =>
+					{ 
+						//setTimeout(function() 
+						//{
+							var searchField = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[1]/div[4]/ppm-common-search/div/div/div/div/input', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+							if (searchField != null)
+							{
+								console.log("set text:", jobList[elemNum]);
+								//Update text field
+								searchField.value = jobList[elemNum];							
+								
+								//create new event
+								var updateEvent = new Event ('change');	
+								
+								//call newly created event !						
+								searchField.dispatchEvent(updateEvent); 
+								
+								//2 sec delay to let job table refresh
+								setTimeout(function() 
+								{								
+									var isLoaded = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[2]/div/ppm-split/div[1]/div/ppm-grid-component/div/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div/div/div[1]/div[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+									if (isLoaded)
+									{	
+										//check if first table entry equals our job name
+										if (isLoaded.textContent.trim() == jobList[elemNum])
+										{
+											console.log("first row text: ", isLoaded.textContent);
+											
+											var selectField = document.evaluate('/html/body/div[3]/div[2]/div[3]/div/div/div/tabs-scrollable/div/div/div/div/div[1]/div/div/div[2]/div[1]/div/div/div/div[2]/common-view-component/div/div[2]/div/ppm-split/div[1]/div/ppm-grid-component/div/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div/div/div/div[1]/div/div/div/div[2]/input', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+											if (selectField != null)
+											{	
+												//select our job !
+												console.log("clicked", new Date().toLocaleTimeString());
+												selectField.click();											
+												resolve('resolved');
+											}										
+										}
+										else
+										{
+											console.log("wrong text value");										
+										}
+									}
+								}, 2000);
+							}	
+						//}, 1500);
+					});					
+				}
+				
+				
+				
+				
+				//var resolveResult1 = await clickFirst();
+				
+				
+				//var resolveResult2 = await clickSecond();				
+				
+				
+				if (jobList.length == 0)
+				{
+					alert(" JobList is empty. \n \rTo create job list go to timesheet you want to copy and use Save Job List ");					
+					return;
+				}
+				
+				do 
+				{
+					var resolveResult3 = await mainFunc((jobList.length - 1));
+					
+					console.log(resolveResult3);
+					
+					jobList.pop();					
+					
+					console.log("jobs left: ", jobList.length);
+					
+					
+				} while (jobList.length > 0);
+				
+			
+				clickAdd();
+				
+
+                
+            }
+
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: populate
+            });
+        });
+    });
+	}});
